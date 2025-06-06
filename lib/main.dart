714de95ff7bce77feb5f5277a60f93ba28baf13b
Ahmed Silini <ahmedsilini5@gmail.com>
2025-06-06 11:44:31 +0100

    setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/core/constants/constants.dart';
+import 'package:tiktok_clone/firebase_options.dart';
 import 'package:tiktok_clone/views/screens/auth/login_screen.dart';
 
-void main() {
-  runApp(
-    ProviderScope(
-      child: const MainApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -20,7 +20,6 @@ class MainApp extends StatelessWidget {
       title: 'Video Feed App',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: AppColors.backgroundColor,
-        
       ),
       home: LoginScreen(),
     );
