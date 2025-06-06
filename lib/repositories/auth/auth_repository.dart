cdd2b2d877561af80a81a0b04729c2e665c299e7
Ahmed Silini <ahmedsilini5@gmail.com>
2025-06-06 10:41:12 +0100

    create user repo

---
+++ lib/repositories/auth/auth_repository.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/models/auth/user_model.dart';
+import 'package:tiktok_clone/core/providers/firebase_providers.dart';
+
+class AuthRepository {
+  final FirebaseAuth _auth;
+  final FirebaseFirestore _firestore;
+
+  AuthRepository(this._auth, this._firestore);
+
+  Stream<AppUser?> get authStateChanges {
+    return _auth.authStateChanges().asyncMap((user) async {
+      if (user == null) return null;
+      return AppUser.fromFirebase(user);
+    });
+  }
+
+  Future<void> signInAnonymously() async {
+    try {
+      final userCredential = await _auth.signInAnonymously();
+      await _createUserProfile(userCredential.user!);
+    } catch (e) {
+      throw Exception('Failed to sign in anonymously: $e');
+    }
+  }
+
+  Future<void> _createUserProfile(User user) async {
+    await _firestore.collection('users').doc(user.uid).set({
+      'uid': user.uid,
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+  }
+}
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  return AuthRepository(
+    ref.read(firebaseAuthProvider),
+    ref.read(firestoreProvider),
+  );
+});
