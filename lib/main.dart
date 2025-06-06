b5776c2c8ce3a551755b120d48b977f961aeee08
Ahmed Silini <ahmedsilini5@gmail.com>
2025-06-06 08:39:00 +0100

    changed background color
    + exported all constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/core/constants/constants.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Video Feed App',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
       ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
