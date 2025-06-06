1a53d78abbf5730eef98a3d2ca71d9598d9cd4c4
Ahmed Silini <ahmedsilini5@gmail.com>
2025-06-06 10:34:54 +0100

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,13 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AppUser {
   final String uid;
-  final String email;
-  final String username; 
+  final String? email;
+  final String? displayName;
+  final bool isAnonymous;
 
   AppUser({
     required this.uid,
-    required this.email,
-    required this.username, 
+    this.email,
+    this.displayName,
+    this.isAnonymous = false,
   });
 
-
+  factory AppUser.fromFirebase(User user) {
+    return AppUser(
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      isAnonymous: user.isAnonymous,
+    );
+  }
 }
