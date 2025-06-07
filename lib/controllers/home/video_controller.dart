33ec4c96f206ebe7e0465091859e1037b9c13b64
Ahmed Silini <ahmedsilini5@gmail.com>
2025-06-07 10:08:20 +0100

    removed useless imports

--- lib/controllers/home/video_controller.dart
+++ lib/controllers/home/video_controller.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tiktok_clone/core/providers/firebase_providers.dart';
 import 'package:tiktok_clone/models/home/video_model.dart';
 import 'package:tiktok_clone/repositories/home/video_repository.dart';
 
