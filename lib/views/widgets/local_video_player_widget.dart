12c28dd0243c1981d4bf1ae50318689f2c399044
Ahmed Silini <ahmedsilini5@gmail.com>
2025-06-07 01:47:06 +0100

    Create local_video_player_widget.dart

---
+++ lib/views/widgets/local_video_player_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class LocalVideoPlayer extends StatefulWidget {
+  final String assetPath;
+
+  const LocalVideoPlayer({super.key, required this.assetPath});
+
+  @override
+  State<LocalVideoPlayer> createState() => _LocalVideoPlayerState();
+}
+
+class _LocalVideoPlayerState extends State<LocalVideoPlayer> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _initVideo();
+  }
+
+  Future<void> _initVideo() async {
+    _controller = VideoPlayerController.asset(widget.assetPath)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+        _controller.setLooping(true);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? AspectRatio(
+          aspectRatio: _controller.value.aspectRatio,
+          child: VideoPlayer(_controller),
+        )
+        : const Center(child: CircularProgressIndicator());
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
